6831719fa6df177a58fe80e40152c8818e4f5aa1
Alexander Leibzon <alexander.leibzon@gmail.com>
2018-08-26 00:33:10 +0300

    add chapter7 code

---
+++ chapter7/mockup_funnel.sql
@@ -0,0 +1,19 @@
+SELECT 'Bought Stop Watch' AS fun_step,
+       10000 AS
+VALUES
+UNION
+  (SELECT 'Bought Running Shoes' AS fun_step,
+          8000 AS
+   VALUES)
+UNION
+  (SELECT 'Bought Running Shorts' AS fun_step,
+          7500 AS
+   VALUES)
+UNION
+  (SELECT 'Bought Running Shirt' AS fun_step,
+          5500 AS
+   VALUES)
+UNION
+  (SELECT 'Started Running' AS fun_step,
+          3000 AS
+   VALUES)
---
+++ chapter7/mockup_map.sql
@@ -0,0 +1,15 @@
+SELECT 32.0853 AS lat,
+       34.7818 AS lon,
+       'Tel Aviv' AS city
+UNION
+  (SELECT 37.9838 AS lat,
+          23.7275 AS lon,
+          'Athens' AS city)
+UNION
+  (SELECT 40.8518 AS lat,
+          14.2681 AS lon,
+          'Napoli' AS city)
+UNION
+  (SELECT 19.0760 AS lat,
+          72.8777 AS lon,
+          'Mumbay' AS city)
